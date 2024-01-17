dfcc2018f389322ebd364d5c52f587920b2dec7e
KoheiTerajima1994 <torippy9pb@gmail.com>
2024-01-18 08:57:08 +0900

    メールアドレス変更しようとした

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -3,80 +3,93 @@
 import React, { useState, MouseEvent, useEffect } from 'react';
 import '../styles/globals.css';
 import { useRouter } from "next/navigation";
-import { onAuthStateChanged, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { onAuthStateChanged, sendEmailVerification, updateEmail } from 'firebase/auth';
 import { auth } from '@/libs/firebase';
 import { FirebaseError } from 'firebase/app';
 import Link from 'next/link';
 
 const MyPage = () => {
-    // TOPページに戻る用
+    // TOPページに戻る処理
     const router = useRouter();
-    const [registerUserName, setRegisterUserName] = useState<string>("");
-
     useEffect(() => {
         // ログイン状態の変更を検知する
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if(!user) {
                 router.push('/login');
             }
-        },[router]);
-
+        });
         return () => unsubscribe();
-    })
+    },[]);
 
-    const handleRegisterUserName = async () => {
+    // アドレスを変更する処理
+    const [changeEmail, setChangeEmail] = useState<string>("");
+
+    const handleClickChangeEmail = async (e: MouseEvent<HTMLAnchorElement>) => {
+        // aタグ デフォルトの処理を防ぐ
+        e.preventDefault();
         try {
-            const user = auth.currentUser;
-            if(!user) {
-                alert('ログインしていません。');
-                return;
-            }
-            await updateProfile(user, {
-                displayName: registerUserName,
-            });
-            alert('アカウント名が設定されました。');
-        } catch(error) {
-            alert('エラーが発生しました。');
+            updateEmail(auth.currentUser, changeEmail);
+
+        // 新しいメールアドレスにメール確認を送信
+        const user = auth.currentUser;
+        await sendEmailVerification(user);
+
+        // ユーザーにメールの確認を促す
+        alert("メールを確認してください。");
+        } catch (error) {
+            alert("【エラー】アドレスの変更ができませんでした。");
             console.error(error);
         }
     }
+
+    // 新しいメールアドレスに対してメール確認を送信する関数
+const sendEmailVerification = async (user: User) => {
+    try {
+        await sendEmailVerification(user.auth);
+    } catch (error) {
+        console.error("メール確認の送信に失敗しました。", error);
+        throw error;
+    }
+}
+
+
+
+
+// // const auth = getAuth();
+// updateEmail(auth.currentUser, "user@example.com").then(() => {
+//   // Email updated!
+//   // ...
+// }).catch((error) => {
+//   // An error occurred
+//   // ...
+// });
+
 
     return (
         <div className="login-page-bg">
             <div className="login-page">
                 <p>みんなの<br />思い出MAP</p>
-                <div style={{display: "flex"}}> 
-                    <div>
-                        <div className="input-wrapper">
-                            <label htmlFor="accountName">アカウント名登録</label>
-                            <input type="text" id="accountName" value={registerUserName} onChange={(e) => setRegisterUserName(e.target.value)} />
-                        </div>
-                        <div className="login-btn-wrapper">
-                            <a href="" className="login-btn" onClick={handleRegisterUserName}>アカウント名登録</a>
-                        </div>
+                <div> 
+                    <div className="input-wrapper">
+                        <label htmlFor="accountNameChange">アカウント名変更</label>
+                        <input type="text" id="accountNameChange" />
                     </div>
-                    <div>
-                        <div className="input-wrapper">
-                            <label htmlFor="accountNameChange">アカウント名変更</label>
-                            <input type="text" id="accountNameChange" />
-                        </div>
-                        <div className="login-btn-wrapper">
-                            <a href="" className="login-btn">アカウント名変更</a>
-                        </div>
-                        <div className="input-wrapper">
-                            <label htmlFor="mailChange">メールアドレス変更</label>
-                            <input type="text" id="mailChange" />
-                        </div>
-                        <div className="login-btn-wrapper">
-                            <a href="" className="login-btn">メールアドレス変更</a>
-                        </div>
-                        <div className="input-wrapper">
-                            <label htmlFor="passwordChange">パスワード変更</label>
-                            <input type="text" id="passwordChange" />
-                        </div>
-                        <div className="login-btn-wrapper">
-                            <a href="" className="login-btn">パスワード変更</a>
-                        </div>
+                    <div className="login-btn-wrapper">
+                        <a href="" className="login-btn">アカウント名変更</a>
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="mailChange">メールアドレス変更</label>
+                        <input type="text" id="mailChange" value={changeEmail} onChange={(e) => setChangeEmail(e.target.value)} />
+                    </div>
+                    <div className="login-btn-wrapper">
+                        <a href="" className="login-btn" onClick={handleClickChangeEmail}>メールアドレス変更</a>
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="passwordChange">パスワード変更</label>
+                        <input type="text" id="passwordChange" />
+                    </div>
+                    <div className="login-btn-wrapper">
+                        <a href="" className="login-btn">パスワード変更</a>
                     </div>
                 </div>
                 <div className="top-btn-wrapper">
