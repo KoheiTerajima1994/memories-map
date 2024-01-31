1a65b7e52e49934282a2ae25ef697241cf26bac8
KoheiTerajima1994 <torippy9pb@gmail.com>
2024-02-01 08:29:11 +0900

    2/1 8:30

--- libs/googleMap.tsx
+++ libs/googleMap.tsx
@@ -186,8 +186,22 @@ const name = useAccountName();
           const containerPostingName: string = doc.data().name;
           const containerPostingText: string = doc.data().text;
           const containerPostingId: string = doc.data().id;
+
+          // 時間表記のフォーマットを行う
+          const parsedDate = new Date(containerPostingDateAndTime);
+          const formatDate = (date: Date) => {
+            const year = date.getFullYear();
+            const month = (date.getMonth() + 1).toString().padStart(2, '0');
+            const day = date.getDate().toString().padStart(2, '0');
+            const hours = date.getHours().toString().padStart(2, '0');
+            const minutes = date.getMinutes().toString().padStart(2, '0');
+
+            return `${year}/${month}/${day} ${hours}:${minutes}`;
+          }
+          const formattedDate = formatDate(parsedDate);
+
           userInformation.push({
-            dateAndTime: containerPostingDateAndTime,
+            dateAndTime: formattedDate,
             name: containerPostingName,
             text: containerPostingText,
             id: containerPostingId,
